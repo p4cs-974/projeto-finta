17af704fad3124aec901d82c48ab56dc286f2a9b
Pedro Custódio <pecustodio030703@gmail.com>
2026-02-12 01:09:52 -0300

    teste deploy com @types/node adicionado

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -12,8 +12,7 @@ const app = new Elysia()
     }),
   });
 
-// aidioncei esse process !== "undefined" pq preciso dele pra poder dar deploy no vercel usando o bun, já q a gente fiz essa condicional do !process.env.
-if (typeof process !== "undefined" && !process.env.VERCEL) {
+if (!process.env.VERCEL) {
   app.listen(3000);
   console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
